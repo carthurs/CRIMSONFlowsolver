f2c12eb4c2bc2088fe5035227f3f4f09ee7990ed
Chris Arthurs <christopher.arthurs@kcl.ac.uk>
2015-02-02 14:19:43 +0000

    New test pack with nothing done in class constructor: testOrphans

---
+++ flowsolver/test/testOrphans.cxx
@@ -0,0 +1,81 @@
+#include "gtest/gtest.h"
+#include "testOrphans.hxx"
+#include "fileReaders.hxx"
+#include "fortranPointerManager.hxx"
+#include "NetlistBoundaryCondition.hxx"
+#include <stdexcept>
+
+// Hack to force the compiler to link this test to the relevant main() for testing
+int PullInMyLibraryTestOrphans() { return 0; }
+
+// The component at the 3D interface must be oriented so that the 3D domain is connected
+// to its startNode (not its endNode). Which is which is determined by the order of the
+// two node indices given in netlist_surfaces.dat forthe component.
+TEST_F(testOrphans, checkNetlistDetectsBad3DInterfaceComponentOrientation) {
+  overrideMissingDataForTesting();
+
+  // Create fake (i.e. non-FORTRAN) pointer manager
+  fortranPointerManager_instance = fortranBoundaryDataPointerManager::Get();
+
+  fortranPointerManager_instance->boundaryFlows.clear();
+  fortranPointerManager_instance->boundaryPressures.clear();
+
+  double fakeFlow = 0.0;
+  double fakePressure = 0.0;
+  // Insert fake pointer data:
+  fortranPointerManager_instance->boundaryFlows.insert(std::pair<int,double*>(2,&fakeFlow));
+
+  fortranPointerManager_instance->boundaryPressures.insert(std::pair<int,double*>(2,&fakePressure));
+
+  // Setup the netlist reader:
+  netlistReader_instance = netlistReader::Instance();
+  netlistReader_instance->setFileName("netlist_surfaces_bad3DInterfaceComponentOrientation.dat");
+  netlistReader_instance->readAndSplitMultiSurfaceInputFile();
+
+  std::vector<std::pair<int,std::string>> surfaceList;
+  surfaceList.clear();
+
+  // Describe test BC that we want to construct:
+  surfaceList.push_back(std::pair <int,std::string> (2,"netlist"));
+
+  // get the boundary condition manager
+  boundaryConditionManager_instance = boundaryConditionManager::Instance();
+  // Testing for "EE: The netlist component at the 3D interface must be connected to it via its start node, not its end node."
+  EXPECT_THROW(boundaryConditionManager_instance->setSurfaceList(surfaceList), std::runtime_error);
+  // boundaryConditionManager_instance->setSurfaceList(surfaceList);
+}
+
+// The component at the 3D interface must have a node which connects to no other components (i.e. just to the 3D domain)
+TEST_F(testOrphans, checkNetlistDetectsBadComponentAt3DInterface) {
+  overrideMissingDataForTesting();
+  
+  // Create fake (i.e. non-FORTRAN) pointer manager
+  fortranPointerManager_instance = fortranBoundaryDataPointerManager::Get();
+
+  fortranPointerManager_instance->boundaryFlows.clear();
+  fortranPointerManager_instance->boundaryPressures.clear();
+
+  double fakeFlow = 0.0;
+  double fakePressure = 0.0;
+  // Insert fake pointer data:
+  fortranPointerManager_instance->boundaryFlows.insert(std::pair<int,double*>(2,&fakeFlow));
+
+  fortranPointerManager_instance->boundaryPressures.insert(std::pair<int,double*>(2,&fakePressure));
+
+  // Setup the netlist reader:
+  netlistReader_instance = netlistReader::Instance();
+  netlistReader_instance->setFileName("netlist_surfaces_badComponentAt3DInterface.dat");
+  netlistReader_instance->readAndSplitMultiSurfaceInputFile();
+
+  std::vector<std::pair<int,std::string>> surfaceList;
+  surfaceList.clear();
+
+  // Describe test BC that we want to construct:
+  surfaceList.push_back(std::pair <int,std::string> (2,"netlist"));
+
+  // get the boundary condition manager
+  boundaryConditionManager_instance = boundaryConditionManager::Instance();
+  // Testing for "EE: Only one component may be directly connected to the 3D interface in the Netlist."
+  EXPECT_THROW(boundaryConditionManager_instance->setSurfaceList(surfaceList), std::runtime_error);
+  // boundaryConditionManager_instance->setSurfaceList(surfaceList);
+}
---
+++ flowsolver/test/testOrphans.hxx
@@ -0,0 +1,78 @@
+#ifndef TESTORPHANS_HPP_
+#define TESTORPHANS_HPP_
+
+#include "multidom.hxx"
+#include "fileReaders.hxx"
+#include "boundaryConditionManager.hxx"
+#include "gtest/gtest.h"
+#include "common_c.h"
+#include <typeinfo>
+#include "debuggingToolsForCpp.hxx"
+#include <boost/shared_ptr.hpp>
+
+	// The fixture for testing class Foo.
+	class testOrphans : public ::testing::Test {
+
+	protected:
+	  // You can remove any or all of the following functions if its body
+	  // is empty.
+	  netlistReader* netlistReader_instance;
+	  boundaryConditionManager* boundaryConditionManager_instance;
+	  fortranBoundaryDataPointerManager* fortranPointerManager_instance;
+
+	  testOrphans() {
+	  }
+
+	  virtual ~testOrphans() {
+	    // You can do clean-up work that doesn't throw exceptions here.
+	    // (*fortranPointerManager_instance).~fortranBoundaryDataPointerManager();
+	  }
+
+	  // If the constructor and destructor are not enough for setting up
+	  // and cleaning up each test, you can define the following methods:
+
+	  virtual void SetUp() {
+	    // Code here will be called immediately after the constructor (right
+	    // before each test).
+		
+	  }
+
+	  virtual void TearDown() {
+	    // Code here will be called immediately after each test (right
+	    // before the destructor).
+	    // multidom_finalise();
+	    // fortranPointerManager_instance->tearDown();
+	    // retrievedBoundaryConditions = 0;
+	    netlistReader_instance->Term();
+	    boundaryConditionManager_instance->Term();
+	    fortranPointerManager_instance->tearDown();
+	  }
+
+	  void overrideMissingDataForTesting() {
+	  	// This function is to be used to create fake data to avoid testing problems
+	  	// due to no solver.inp being read prior to testing some of the smaller features.
+	  	//
+	  	// Please document each override very carefully, and write a note to the console!
+
+	  	// This is because timdat.lstep is used to determine whether we're restarting a simulation
+	  	// and we're not doing that during tests.
+	  	std::cout << "Information -- I'm overriding the following variables for this test..." << std::endl;
+	  	std::cout << "Information -- The overrides are in testOrphans.hxx." << std::endl;
+	  	std::cout << " timdat.lstep" << std::endl;
+	  	timdat.lstep = int(0);
+	  	std::cout << " inpdat.Delt[0]" << std::endl;
+	  	inpdat.Delt[0] = 0.01;
+	  	// grcrbccom.numGRCRSrfs;
+	  	// std::cout << "nomodule.numNetlistLPNSrfs" << std::endl;
+    //     nomodule.numNetlistLPNSrfs = int(1);
+        // nomodule.numControlledCoronarySrfs;
+	  }
+
+	  // Objects declared here can be used by all tests in the test case for Foo.
+	};
+
+// Hack to force the compiler to link this test to the relevant main() for testing
+	int PullInMyLibraryTestOrphans();
+
+#endif
+	

ee803e901b18e63b02fb2359d28507b35ea881d5
Chris Arthurs <christopher.arthurs@kcl.ac.uk>
2015-12-14 14:04:41 +0000

    added missing test files

---
+++ testbin/mainTests/zeroDDomain/pythonPicklers/resistanceController.py
@@ -0,0 +1,33 @@
+from CRIMSONPython import *
+from math import pi, cos
+
+class parameterController(abstractParameterController):
+
+	def __init__(self, baseNameOfThisScriptAndOfRelatedFlowOrPressureDatFile, MPIRank):
+		abstractParameterController.__init__(self,baseNameOfThisScriptAndOfRelatedFlowOrPressureDatFile, MPIRank)
+		self.m_periodicTime = 0.0; #\todo think about this for restarts!
+		self.m_timeToMaximumElastance = 0.2782;
+		self.m_timeToRelax = 0.1391;
+		self.m_minimumElastance = 4.10246e-3;
+		self.m_maximumElastance = 3.0827e-1;
+		self.m_heartPeriod = 0.86;
+
+
+	def updateControl(self, currentParameterValue, delt, dictionaryOfPressuresByNodeIndex, dictionaryOfFlowsByComponentIndex, dictionaryOfVolumesByComponentIndex):
+
+		self.updatePeriodicTime(delt)
+		pressure = abs(0.001 * cos(pi*100*self.m_periodicTime))
+
+		# for key in dictionaryOfPressuresByNodeIndex:
+		# 	print "Pressure ", key, " was ", dictionaryOfPressuresByNodeIndex[key]
+		# for key in dictionaryOfFlowsByComponentIndex:
+		# 	print "Flow ", key, " was ", dictionaryOfFlowsByComponentIndex[key]
+
+		return pressure
+
+	def updatePeriodicTime(self, delt):
+	
+		self.m_periodicTime = self.m_periodicTime + delt
+		# Keep m_periodicTime in the range [0,m_heartPeriod):
+		if self.m_periodicTime >= self.m_heartPeriod:
+			self.m_periodicTime = self.m_periodicTime - self.m_heartPeriod

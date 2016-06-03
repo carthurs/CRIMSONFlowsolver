4d2ed13821b33b5005051dfccf51ee503caf06ec
miquel aguirre <miquel.aguirre@gmail.com>
2016-06-03 14:00:06 -0400

    Printing some partial results in e3b and e3bvar for debugging

--- flowsolver/src/e3bvar.f90
+++ flowsolver/src/e3bvar.f90
@@ -167,6 +167,31 @@
          dxdxib(:,3,2) = dxdxib(:,3,2) + xlb(:,n,3) * shglb(:,2,n)
          dxdxib(:,3,3) = dxdxib(:,3,3) + xlb(:,n,3) * shglb(:,3,n)
       enddo
+
+      ! #if DEBUG_ALE == 1 
+      ! write(*,*) 'inside e3bvar then stop'
+
+      ! open(793,file='shglb.dat',status='new')
+      ! do i = 1, npro
+      !     do j = 1, nsd
+      !     write(793,'(2(i10),4(e20.10))') i,j,shglb(i,j,1), shglb(i,j,2), shglb(i,j,3),&
+      !                             shglb(i,j,4) ! nenl = 4
+      !     end do                          
+      ! end do 
+      ! close(793)
+
+      ! open(794,file='shpb.dat',status='new')
+      ! do i = 1, npro
+
+      !     write(794,'(1(i10),4(e20.10))') i,shpb(i,1), shpb(i,2), shpb(i,3),&
+      !                             shpb(i,4)                      
+      ! end do 
+      ! close(794)
+
+      ! ! stop
+
+      ! #endif
+
 !
 !.... compute the normal to the boundary
 !
